4cb1c73bddb3e5e0db1d885ba3baeb38efed2d3e
stevejj4 <jumaomondi16@gmail.com>
2026-01-10 13:06:07 +0300

    first commit

---
+++ src/main/java/com/example/cashcard/CashCard.java
@@ -0,0 +1,5 @@
+package com.example.cashcard;
+
+// A simple immutable data class using Java record
+public record CashCard(Long id, Double amount) {
+}
---
+++ src/main/java/com/example/cashcard/Controller/CashCardController.java
@@ -0,0 +1,24 @@
+package com.example.cashcard.Controller;
+
+import com.example.cashcard.CashCard;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // marks this class as the rest controller
+@RequestMapping("/cashcards") // defining the base URL
+public class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<CashCard> findById(@PathVariable long requestedId) {
+        // @PathVariable -- to grab values from the URL
+        if (requestedId == 99L) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
